08833ed1d2d9b876b563823c42fa0b8604ba9355
Dellaire <henry.borasch@freenet.de>
2017-11-06 18:44:57 +0100

    added calculation of intersection of two vector equations

--- src/main/java/de/bearishcode/math/vector/VectorEquation.java
+++ src/main/java/de/bearishcode/math/vector/VectorEquation.java
@@ -1,5 +1,7 @@
 package de.bearishcode.math.vector;
 
+import de.bearishcode.math.sle.SystemOfLinearEquations;
+
 public class VectorEquation
 {
 	private double[] startPoint;
@@ -17,6 +19,20 @@ public class VectorEquation
 			this.startPoint[i] = startPoint[i];
 			this.direction[i] = endPoint[i] - this.startPoint[i];
 		}
+	}
+
+	public double[] calculateIntersection(VectorEquation vectorEquation)
+	{
+		SystemOfLinearEquations systemOfLinearEquations = new SystemOfLinearEquations(this, vectorEquation);
+		double[] resultVector = systemOfLinearEquations.solve();
+
+		double[] intersection = new double[this.dimension];
+		for (int i = 0; i < this.dimension; i++)
+		{
+			intersection[i] = this.startPoint[i] + (resultVector[i] * this.direction[i]);
+		}
+
+		return intersection;
 	}
 
 	public double[] getStartPoint()
--- src/test/java/de/bearishcode/math/vector/VectorEquationTest.java
+++ src/test/java/de/bearishcode/math/vector/VectorEquationTest.java
@@ -20,4 +20,36 @@ public class VectorEquationTest
 		assertThat(vectorEquation.getDirection()[0], is(1.0));
 		assertThat(vectorEquation.getDirection()[1], is(2.0));
 	}
+
+	@Test
+	public void calculateSimpleIntersection()
+	{
+		double[] startPoint1 = { 0.0, 0.0 };
+		double[] endPoint1 = { 1.0, 1.0 };
+		double[] startPoint2 = { 2.0, 0.0 };
+		double[] endPoint2 = { 5.0, 1.0 };
+
+		VectorEquation vectorEquation1 = new VectorEquation(startPoint1, endPoint1);
+		VectorEquation vectorEquation2 = new VectorEquation(startPoint2, endPoint2);
+		double[] intersection = vectorEquation1.calculateIntersection(vectorEquation2);
+
+		assertThat(intersection[0], is(-1.0));
+		assertThat(intersection[1], is(-1.0));
+	}
+
+	@Test
+	public void calculateHardIntersection()
+	{
+		double[] startPoint1 = { -280.407, 218.915 };
+		double[] endPoint1 = { -309.527, 219.272 };
+		double[] startPoint2 = { -403.049, 725.470 };
+		double[] endPoint2 = { -437.636, 716.125 };
+
+		VectorEquation vectorEquation1 = new VectorEquation(startPoint1, endPoint1);
+		VectorEquation vectorEquation2 = new VectorEquation(startPoint2, endPoint2);
+		double[] intersection = vectorEquation1.calculateIntersection(vectorEquation2);
+
+		assertThat(intersection[0], is(-2191.1719422771926));
+		assertThat(intersection[1], is(237.37164239144636));
+	}
 }
